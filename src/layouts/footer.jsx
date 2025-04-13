2a84cd1e625959c507dd3f3cbb5243dfc3590511
Jaime Brandy <71730269+JaimeCesar1998@users.noreply.github.com>
2025-04-13 15:12:18 +0100

    c

--- src/layouts/footer.jsx
+++ src/layouts/footer.jsx
@@ -1,19 +1,15 @@
 export const Footer = () => {
     return (
-        <footer className="flex flex-wrap items-center justify-between gap-4 pt-4">
-            <p className="text-base font-medium text-slate-900 dark:text-slate-50">© 2024 XD Code All Rights Reserved</p>
+        <footer className="flex flex-wrap items-center justify-between gap-4 pt-4 border-t mt-8 dark:border-slate-800">
+            <p className="text-base font-medium text-slate-900 dark:text-slate-50">
+                © {new Date().getFullYear()} Nkandap - Todos os direitos reservados
+            </p>
             <div className="flex flex-wrap gap-x-2">
-                <a
-                    href="#"
-                    className="link"
-                >
-                    Privacy Policy
+                <a href="#" className="link">
+                    Política de Privacidade
                 </a>
-                <a
-                    href="#"
-                    className="link"
-                >
-                    Terms of Service
+                <a href="#" className="link">
+                    Termos de Uso
                 </a>
             </div>
         </footer>
